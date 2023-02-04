ae8b7400965c5efd48635ddcecdd147c01f6b430
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-02-05 00:09:37 +0300

    fix

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    console.log('Welcome to the Brain Games!');
+    console.log('May I have your name? John');
+    console.log('Hello, John!');
+}
+
+export default greeting;
